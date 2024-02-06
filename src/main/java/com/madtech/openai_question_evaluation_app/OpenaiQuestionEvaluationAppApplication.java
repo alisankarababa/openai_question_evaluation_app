930cc0f8a768d168e0bf22edb4f39740a51b3f7c
Alişan <alisankarababa@gmail.com>
2024-02-06 14:16:32 +0300

    add main app files

---
+++ src/main/java/com/madtech/openai_question_evaluation_app/OpenaiQuestionEvaluationAppApplication.java
@@ -0,0 +1,13 @@
+package com.madtech.openai_question_evaluation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiQuestionEvaluationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaiQuestionEvaluationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/madtech/openai_question_evaluation_app/OpenaiQuestionEvaluationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madtech.openai_question_evaluation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiQuestionEvaluationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
